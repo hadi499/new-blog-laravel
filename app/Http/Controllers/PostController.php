c175efc900f376e8fcd10ffa12dfa71dc9f85636
hadi499 <hadi.ygcl@gmail.com>
2023-05-13 18:55:17 +0700

    ok

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
             'active' => 'posts',
             // 'posts' => Post::all()
             // 'posts' => Post::latest()->get()
-            'posts' => $posts->get()
+            'posts' => $posts ->get()
         ]);
     }
 
